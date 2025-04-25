9ea71a8e9f6bf497e642c0955f84bde897592770
Tarik Lafci <tariklafci@gmail.com>
2025-04-25 23:58:58 +0300

    added fetching appointments for clinics

--- petlyst-mobile-backend/routes/appointmentRoutes.js
+++ petlyst-mobile-backend/routes/appointmentRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const multer = require('multer');
 const upload = multer(); // Or configure as needed
 const authenticateToken = require('../middlewares/authenticateToken');
-const { createAppointment, fetchAppointments } = require('../controllers/appointmentController');
+const { createAppointment, fetchAppointments, fetchAppointmentsClinics } = require('../controllers/appointmentController');
 
 
 router.post('/create-appointment', authenticateToken, createAppointment);
 router.get('/fetch-appointments', authenticateToken, fetchAppointments);
+router.get('/fetch-appointments-clinics', authenticateToken, fetchAppointmentsClinics);
+
 
 module.exports = router;
