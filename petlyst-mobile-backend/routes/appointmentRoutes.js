8c106120f8cda304353316a25c2f70a4701a57e3
Tarik Lafci <tariklafci@gmail.com>
2025-04-26 00:07:11 +0300

    typo fixed

--- petlyst-mobile-backend/routes/appointmentRoutes.js
+++ petlyst-mobile-backend/routes/appointmentRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const multer = require('multer');
 const upload = multer(); // Or configure as needed
 const authenticateToken = require('../middlewares/authenticateToken');
-const { createAppointment, fetchAppointments, fetchAppointmentsClinics } = require('../controllers/appointmentController');
+const { createAppointment, fetchAppointments, fetchAppointmentsClinic } = require('../controllers/appointmentController');
 
 
 router.post('/create-appointment', authenticateToken, createAppointment);
 router.get('/fetch-appointments', authenticateToken, fetchAppointments);
-router.get('/fetch-appointments-clinics', authenticateToken, fetchAppointmentsClinics);
+router.get('/fetch-appointments-clinics', authenticateToken, fetchAppointmentsClinic);
 
 
 module.exports = router;
