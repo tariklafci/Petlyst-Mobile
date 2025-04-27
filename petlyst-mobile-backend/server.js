42fd04a021b51f760664f7a84275571ab78d5ad7
Tarik Lafci <tariklafci@gmail.com>
2025-04-27 17:18:42 +0300

    jitsi conf backend

--- petlyst-mobile-backend/server.js
+++ petlyst-mobile-backend/server.js
@@ -9,7 +9,7 @@ const authRoutes = require('./routes/authRoutes');
 const petRoutes = require('./routes/petRoutes');
 const clinicRoutes = require('./routes/clinicRoutes');
 const appointmentRoutes = require('./routes/appointmentRoutes');
-const jitsiRoutes = require('./routes/jitsi');
+const jitsiRoutes = require('./routes/jitsiRoutes');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -28,7 +28,7 @@ app.use('/api', authRoutes);
 app.use('/api', petRoutes);
 app.use('/api', clinicRoutes);
 app.use('/api', appointmentRoutes);
-app.use('/jitsi', jitsiRoutes);
+app.use('/api', jitsiRoutes);
 
 const httpsServer = https.createServer(credentials, app);
 
