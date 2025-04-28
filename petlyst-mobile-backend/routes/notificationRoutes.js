8155597f08b710839af9bdd68b0f351f7c6df192
Tarik Lafci <tariklafci@gmail.com>
2025-04-29 02:33:13 +0300

    typo

--- petlyst-mobile-backend/routes/notificationRoutes.js
+++ petlyst-mobile-backend/routes/notificationRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { testSendAppointmentNotifications } = require('../controllers/notificationController');
 
 
-app.get('/api/test-notifications', testSendAppointmentNotifications);
+router.get('/api/test-notifications', testSendAppointmentNotifications);
 
 module.exports = router;
 
