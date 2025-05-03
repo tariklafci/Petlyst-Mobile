d99f4f1a7072d0a1bcf284f550125c898dceb34d
Tarik Lafci <tariklafci@gmail.com>
2025-05-03 23:04:43 +0300

    imported authenticateToken to ollamaRoutes

--- petlyst-mobile-backend/routes/ollamaRoutes.js
+++ petlyst-mobile-backend/routes/ollamaRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { generateResponse } = require('../controllers/ollamaController');
+const authenticateToken = require('../middlewares/authenticateToken');
+
 const router = express.Router();
 
 router.post('/generate-response', authenticateToken, generateResponse);
